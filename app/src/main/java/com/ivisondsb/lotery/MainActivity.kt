bdda7ec22808b1292bead915699666cfeba4732d
Ivison <ivison.dev@gmail.com>
2025-09-27 22:35:05 -0300

    refactor code equals class 1

--- app/src/main/java/com/ivisondsb/lotery/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/lotery/MainActivity.kt
@@ -8,19 +8,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -57,34 +56,37 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 @Composable
 fun CardLottery(name: String, modifier: Modifier = Modifier) {
     Card(
-        modifier = modifier
-            .wrapContentSize()
-            .shadow(10.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = modifier.wrapContentSize()
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier
+                .wrapContentSize()
                 .background(Green)
-                .padding(20.dp)
         ) {
             Image(
                 painter = painterResource(R.drawable.trevo),
                 contentDescription = "Trevo",
-                modifier.size(100.dp)
+                modifier
+                    .size(100.dp)
+                    .padding(10.dp)
             )
-            Spacer(modifier.size(10.dp))
             Text(
                 name,
                 style = TextStyle(
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
                     color = Color.White
-                )
+                ),
+                modifier = modifier.padding(8.dp, 0.dp, 8.dp, 8.dp)
             )
         }
     }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
