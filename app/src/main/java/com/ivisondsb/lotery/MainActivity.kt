a6e36f370a5653b82ed39a0c6a23852c6d774912
Ivison <ivison.dev@gmail.com>
2025-09-30 11:45:24 -0300

    create the form screen design

--- app/src/main/java/com/ivisondsb/lotery/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/lotery/MainActivity.kt
@@ -9,22 +9,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -73,9 +85,67 @@ fun HomeScreen(modifier: Modifier = Modifier, onClick: () -> Unit) {
 fun FormScreen(modifier: Modifier = Modifier) {
     Surface(
         modifier.fillMaxSize(),
-        color = Color.Green
+        color = MaterialTheme.colorScheme.background
     ) {
+        var qtdNumbers by remember { mutableStateOf("") }
+        var qtdBets by remember { mutableStateOf("") }
 
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Spacer(modifier.size(20.dp))
+            Image(
+                painter = painterResource(R.drawable.trevo),
+                contentDescription = stringResource(R.string.clover),
+                modifier
+                    .size(100.dp)
+                    .padding(10.dp)
+            )
+            Text(
+                text = "Mega Sena",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.announcement),
+                fontStyle = FontStyle.Italic,
+                modifier = modifier.wrapContentSize().padding(14.dp),
+            )
+            OutlinedTextField(
+                value = qtdNumbers,
+                maxLines = 1,
+                label = {
+                    Text(stringResource(id = R.string.mega_rule))
+                },
+                placeholder = {
+                    Text(stringResource(id = R.string.quantity))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                onValueChange = {}
+            )
+            OutlinedTextField(
+                value = qtdBets,
+                maxLines = 1,
+                label = {
+                    Text(stringResource(R.string.bets))
+                },
+                placeholder = {
+                    Text(stringResource(id = R.string.bets_quantity))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                onValueChange = {}
+            )
+            OutlinedButton(onClick = {}) {
+                Text(stringResource(R.string.bets_generate))
+            }
+        }
     }
 }
 
@@ -98,7 +168,7 @@ fun CardLottery(name: String, onClick: () -> Unit, modifier: Modifier = Modifier
         ) {
             Image(
                 painter = painterResource(R.drawable.trevo),
-                contentDescription = "Trevo",
+                contentDescription = stringResource(R.string.clover),
                 modifier
                     .size(100.dp)
                     .padding(10.dp)
